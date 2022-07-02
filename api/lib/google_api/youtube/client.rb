7cc9eab7b42ef450c9318dfb749205a132814b83
Kanon159 <19369951+Kanon159@users.noreply.github.com>
2022-07-03 02:28:27 +0900

    Update: APIにYouTubeの動画データ取得処理追加

--- api/lib/google_api/youtube/client.rb
+++ api/lib/google_api/youtube/client.rb
@@ -1,10 +1,18 @@
-# frozen_string_literal: true
-
-require 'google/apis/youtube_v3'
+require "google/apis/youtube_v3"
 
 module GoogleApi
   module Youtube
     class Client
+      def initialize
+        @app_key = Rails.application.credentials.youtube[:api_key]
+      end
+
+      def videos(video_id)
+        client = Google::Apis::YoutubeV3::YouTubeService.new
+        client.key = @app_key
+        parts = %w(id snippet liveStreamingDetails player statistics status)
+        client.list_videos(parts.join(","), id: video_id)
+      end
     end
   end
 end
